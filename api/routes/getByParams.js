70e4e03f10e4e6e8ba3450c84262d07b7d57fda1
AvidDabbler <walter.k.jenkins@gmail.com>
2020-12-03 08:17:07 -0600

    fixed filter issue on tree and ward filters

--- api/routes/getByParams.js
+++ api/routes/getByParams.js
@@ -1,58 +1,46 @@
 var express = require('express');
 var router = express.Router();
 const trees = require("../data/liquidambarTrees_DC.json");
+const geojson = require("../data/geojson.json");
 
 const dataRequest = async (req, res)=>{
-    console.log(res.headers);
-
-    const inList = (list, features, field) => {
-        features.filter((i, n) => {
-            if(list.indexOf(n.properties[field]) >= 0) {
-                return 
-            }
-        })
-    }
-
-    const treeFilter = (t) => {
-        const treeList = req.headers.trees;
-        const wardList = req.headers.wards;
-        let tFiltered;
+    
+    const treeFilter = () => {
+        let tFiltered = geojson;
 
         // kick out all features if tree or ward query not in header
-        if(!treeList && !wardList){
-            console.log('here')
-            console.log(treeList)
-            return t
+        if(!req.headers.condition && !req.headers.wards){
+            return {error: "You need to use either the Wards or Condition parameter"};
         }
-        // filter by wards. query should be a list of wards
-        if (wardList) {
-            t['features'] = () => {
-                t.features.filter((i, n) => {
-                    if(treeList.indexOf(n.properties.ward) >= 0) {
-                        return n
-                    }
-                })
-            }
-        }
+
+        // split tree and condition parameters into lists
+        let conditionList = req.headers.condition ? req.headers.condition.split(',') : undefined
+        let wardList = req.headers.wards ? req.headers.wards.split(',') : undefined
+
         // filter by tree condition
-        if (treeList) {
-
-            console.log(treeList.split(','))
-
-            t['features'] = 
-                    t['features'].filter((i) => {
-                        // console.log(i.properties)
-                        if(treeList.split(',').indexOf(i.properties.CONDITION) >= 0) {
-                            return i
+        if (conditionList) {
+            trees.features.forEach(feature => {
+                if(conditionList.indexOf(feature.properties.CONDITION) >= 0) {
+                    // Check for and filter by wards
+                    if (wardList) {
+                        console.log(wardList)
+                        // look to see if feature.WARD is in the wardList
+                        if(wardList.indexOf(feature.properties.WARD.toString()) >= 0) {
+                            tFiltered.features.push(feature)
                         }
-                    })
+                    }
+                    // push feature to list because condition is in list
+                    else {
+                        tFiltered.features.push(feature)
+                    }
                 }
-        
-        return t
+            });
+        }
+
+        return tFiltered;
     }
 
-    let data = treeFilter(trees)
-    res.send({trees: data})
+    res.send({trees: await treeFilter()});
     
 };
 
