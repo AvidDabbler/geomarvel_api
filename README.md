d8a30d38b52ce866426d677dd8fbd736214ddb3a
AvidDabbler <walter.k.jenkins@gmail.com>
2020-12-03 08:56:30 -0600

    deleted ward fiter & started readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# geomarvel_api
+# DC Trees Api
+## About
+This Api was written to serve up a geojson file that has all of the DC Trees and its associated attributes touching on condition, built environment, and inspection notes. 
+
+This api has 2 different calls that can be made:
+- /getAll
+- /getByParams
+
+# /getAll
+The /getAll command will return all of the features from the geojson file.
+
+# /getByParams
+The /getAll command allows for the user to return a filtered set of trees based on the Condition of the trees. These conditions need to be passed in the header of the request as a string. If multiple tree conditions are needed they should be sent as a string separated by a comma (ex. "Good,Poor")
+The Ward and Condition
--- api/routes/getByParams.js
+++ api/routes/getByParams.js
@@ -15,24 +15,12 @@ const dataRequest = async (req, res)=>{
 
         // split tree and condition parameters into lists
         let conditionList = req.headers.condition ? req.headers.condition.split(',') : undefined
-        let wardList = req.headers.wards ? req.headers.wards.split(',') : undefined
 
         // filter by tree condition
         if (conditionList) {
             trees.features.forEach(feature => {
                 if(conditionList.indexOf(feature.properties.CONDITION) >= 0) {
-                    // Check for and filter by wards
-                    if (wardList) {
-                        console.log(wardList)
-                        // look to see if feature.WARD is in the wardList
-                        if(wardList.indexOf(feature.properties.WARD.toString()) >= 0) {
-                            tFiltered.features.push(feature)
-                        }
-                    }
-                    // push feature to list because condition is in list
-                    else {
-                        tFiltered.features.push(feature)
-                    }
+                    tFiltered.features.push(feature)
                 }
             });
         }
