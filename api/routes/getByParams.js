07ccf5bc8f0fb39924f17819e1c35f40713d794d
AvidDabbler <walter.k.jenkins@gmail.com>
2020-12-05 07:53:01 -0600

    filtering by ward and condition

--- api/routes/getByParams.js
+++ api/routes/getByParams.js
@@ -4,7 +4,14 @@ const trees = require("../data/liquidambarTrees_DC.json");
 const geojson = require("../data/geojson.json");
 
 const dataRequest = async (req, res)=>{
-    let query = req.query
+    res.header('Access-Control-Allow-Origin', '*');
+
+    console.log('wards', req.query.WARD)
+    console.log('CONDITION', req.query.CONDITION)
+
+    let query = req.query;
+    let WARD = req.query.WARD ? req.query.WARD : '*' ;
+    let CONDITION = req.query.CONDITION ? req.query.CONDITION : '*' ;
 
     console.log(query)
 
@@ -12,18 +19,21 @@ const dataRequest = async (req, res)=>{
         let tFiltered = geojson;
 
         // kick out all features if tree or ward query not in header
-        if(!req.headers.condition && !req.headers.wards) {
+        if(CONDITION == '*' && WARD == '*') {
             return {error: "You need to use either the Wards or Condition parameter"};
         }
 
         // split tree and condition parameters into lists
-        let conditionList = req.headers.condition ? req.headers.condition.toUpperCase().replace(/\s/g, '').split(',') : undefined
+        let conditionList = CONDITION == "*" ? ["EXECELLENT","GOOD","FAIR","POOR"] : CONDITION.toUpperCase().replace(/\s/g, '').split(',');
+        let wardList = WARD == "*" ? ['1','2','3','4','5','6','7','8','9'] : WARD.toString().replace(/\s/g, '').split(',');
 
         // filter by tree condition
         if (conditionList) {
             trees.features.forEach(feature => {
                 if(conditionList.indexOf(feature.properties.CONDITION.toUpperCase()) >= 0) {
-                    tFiltered.features.push(feature)
+                    if(wardList.indexOf(feature.properties.WARD.toString()) >= 0) {
+                        tFiltered.features.push(feature)
+                    }
                 }
             });
         }
--- app.js
+++ app.js
@@ -8,17 +8,6 @@ const server = http.createServer((req, res) => {
     const method = req.method;
     const data = req.headers.data;
 
-    // switch(url){
-    //     case url.startsWith('/trees'):
-    //         trees.treeRequest(req)
-    //         break;
-    //     case '/wards':
-    //         break;
-    //     default:
-    //         console.log('nothing');
-    //         break;
-    // }
-
     if(method != 'GET'){
         return;
     }
