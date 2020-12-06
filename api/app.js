59129459ec8629c88b749f705d92e192018fa674
AvidDabbler <walter.k.jenkins@gmail.com>
2020-12-05 18:51:26 -0600

    clean up fancy cors attempt

--- api/app.js
+++ api/app.js
@@ -10,22 +10,6 @@ var getByParamsRouter = require('./routes/getByParams');
 
 var app = express();
 
-// const cors = require('cors');
-
-// let whitelist = ['http://192.168.1.107:3001']
-
-// app.use(cors({
-//   origin: function(origin, callback){
-//     // allow requests with no origin 
-//     if(!origin) return callback(null, true);
-//     if(whitelist.indexOf(origin) === -1){
-//       var message = 'error';
-//       return callback(new Error(message), false);
-//     }
-//     return callback(null, true);
-//   }
-// }));
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
